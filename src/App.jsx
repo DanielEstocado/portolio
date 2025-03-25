439aa9ede8bcee80ad69ed48835e141e19093143
Admin <danielestocado143@gmail.com>
2025-03-25 13:02:25 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-900 max-w-screen-xl justify-center mx-auto'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
